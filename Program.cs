dcaf33beb38023c61f855ffd2864ec622b6c2ea6
Gr8V <vanshdangi0045@gmail.com>
2024-04-29 23:42:23 +0530

    Taking User Input

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 
 class Program{
+    public static string[] teams = [];
+    public static int NoOfTeams = 0;
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Inputs();
+        Console.WriteLine(NoOfTeams);
+
+        Console.ReadKey();
+    }
+
+    public static void Inputs(){
+        Console.Write("Enter list of teams(seprated by commas)");
+        string? rawInput =   Console.ReadLine();
+        teams = rawInput.Split(',');
+        NoOfTeams = teams.Length;
     }
 }
--- obj/Debug/net8.0/knockout-tournament-generator.AssemblyInfo.cs
+++ obj/Debug/net8.0/knockout-tournament-generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("knockout-tournament-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22eb3510cc2baf68271b9349e1c310cd881752ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("knockout-tournament-generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("knockout-tournament-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
